21740370402f923d8730c23c23b19402594e4b9d
erwinagpasa <istran.net@gmail.com>
2020-12-12 20:37:58 +0300

    initial release

---
+++ index.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<meta name="generator" content="Hugo 0.79.0">
+	<title>Carousel · Bootstrap v5.0</title>
+	<link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/starter-template/">
+	<link href="css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+	<link href="css/global.css" rel="stylesheet">
+	<link href="css/all.css" rel="stylesheet">
+</head>
+<body>
+	<main class="container-fluid">
+		<div id="wrapper">
+			<div class="col-md-6 mx-auto">
+
+				<h1 class="text-center white">Our Testimonials</h1>
+				<p class="text-center white">Take a look below to learn what our clients are saying about us:</p>
+
+				<div id="carouselExample" class="carousel carousel-light slide" data-bs-ride="carousel">
+					<ol class="carousel-indicators">
+						<li data-bs-target="#carouselExample" data-bs-slide-to="0" class="active"></li>
+						<li data-bs-target="#carouselExample" data-bs-slide-to="1"></li>
+						<li data-bs-target="#carouselExample" data-bs-slide-to="2"></li>
+						<li data-bs-target="#carouselExample" data-bs-slide-to="3"></li>
+					</ol>
+					<div class="carousel-inner">
+
+						<div class="carousel-item active" data-bs-interval="1000">
+							<div class="img-box"><img src="img/testi_1.jpg" alt=""></div>
+							<p class="testimonial">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper malesuada ante. Idac bibendum scelerisque non non purus. Suspendisse varius nibh non aliquet.</p>
+							<p class="overview"><b>John Deo</b>, Media Analyst</p>
+						</div>
+
+
+						<div class="carousel-item" data-bs-interval="2000">
+							<div class="img-box"><img src="img/testi_2.jpg" alt=""></div>
+							<p class="testimonial">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper malesuada ante. Idac bibendum scelerisque non non purus. Suspendisse varius nibh non aliquet.</p>
+							<p class="overview"><b>Paula Wilson</b>, Media Analyst</p>
+						</div>
+
+						<div class="carousel-item">
+							<div class="img-box"><img src="img/testi_3.jpg" alt=""></div>
+							<p class="testimonial">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper malesuada ante. Idac bibendum scelerisque non non purus. Suspendisse varius nibh non aliquet.</p>
+							<p class="overview"><b>Paula Wilson</b>, Media Analyst</p>
+						</div>
+
+
+						<div class="carousel-item">
+							<div class="img-box"><img src="img/testi_4.jpg" alt=""></div>
+							<p class="testimonial">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper malesuada ante. Idac bibendum scelerisque non non purus. Suspendisse varius nibh non aliquet.</p>
+							<p class="overview"><b>Paula Wilson</b>, Media Analyst</p>
+						</div>
+
+					</div>
+
+					<a class="carousel-control-prev" href="#carouselExample" role="button" data-bs-slide="prev">
+						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+						<span class="visually-hidden">Previous</span>
+					</a>
+					<a class="carousel-control-next" href="#carouselExample" role="button" data-bs-slide="next">
+						<span class="carousel-control-next-icon" aria-hidden="true"></span>
+						<span class="visually-hidden">Next</span>
+					</a>
+				</div>
+			</div>
+		</div>
+	</main><!-- /.container -->
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+	<script src="js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+</body>
+</html>
